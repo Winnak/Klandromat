3bc04381e8d0c43b6f62698c81ffaa1c8d84ee4a
Erik Hoyjor <ErikHoyJor@gmail.com>
2017-04-03 17:18:07 +0200

    Hid upcomming klandringer from the accused.

--- source/routes/user.php
+++ source/routes/user.php
@@ -1,6 +1,6 @@
 <h3><?php echo $arguements["name"] ?></h3><a href="/logout">Logout</a> 
 <br>
-<?php 
+<?php
 $db = new mysqli(MYSQL_PROVIDER, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
 $db->set_charset("utf8");
 $id = $arguements["id"];
@@ -11,29 +11,47 @@ $losings = 0;
 $klandring_table = "";
 
 if ($result->num_rows > 0) {
-    $klandring_table .= "<table class='table'><thead><tr><th></th><th></th><th></th><th></th><th></th><th></th></tr></thead><tbody>";
-    while($row = $result->fetch_assoc()) {
-            $klandring_table .= "<tr><td>" . $row["verdictdate"] 
-                                . "</td><td>" . $row["title"]
-                                . "</td><td>" . ($row["verdict"] == 0 ? $row["from"] . "</td style='color: grey'><td>" . $row["to"] . "</td style='color: grey'><td>" . "?":
-                                                ($row["verdict"] == 1 ? "<b style='color: #228822'>" . $row["from"] . "</b></td><td style='color: red'>" . $row["to"] . "</td><td>" . ($row["to"] == $id ? "$" : ""):
-                                                ($row["verdict"] == 2 ? $row["from"] . "</td style='color: red'><td><b style='color: #228822'>" . $row["to"] . "</b>" . "</td><td>" . ($row["to"] == $id ? "" : "$"):
-                                                ($row["verdict"] == 3 ? "<b style='color: #cfcf00'>" . $row["from"] . "</b></td><td><b style='color: #cfcf00'>" . $row["to"] . "</b>" . "</td><td>" . "$":
-                                                "ERROR?!"))))
-                                . "</td><td>" . ($row["paid"] == 1 ? "betalt" : "ikke betalt") . "</td></tr>";
+    $klandring_table .= "<table class='table'><thead><tr><th></th><th></th><th></th><th></th><th></th></tr></thead><tbody>";
+    while ($row = $result->fetch_assoc()) {
+        if ($row["verdict"] === 0 && $row["from"] !== $id) {
+            // Hide upcomming klandringer for the accused.
+            continue;
+        }
+        $from = $row["from"];
+        $to = $row["to"];
+
+        $klandring_table .= "<tr><td>" . $row["verdictdate"] . "</td><td>" . $row["title"] . "</td>";
+        switch ($row["verdict"]) {
+            default:
+            case 0:
+                $klandring_table .= "<td><div class='tie'>$from</div></td><td><div class='tie'>$to</div></td>";
+                break;
+            case 1:
+                $klandring_table .= "<td><div class='loss'>$from $</div></td><td><div class='win'>$to</div></td>";
+                break;
+            case 2:
+                $klandring_table .= "<td><div class='win'>$from</div></td><td><div class='loss'>$to $</div></td>";
+                break;
+            case 3:
+                $klandring_table .= "<td><div class='tie'>$from $</div></td><td><div class='tie'>$to $</div></td>";
+                break;
+        }
+
+        $klandring_table .= "<td>" . ($row["paid"] == 1 ? "betalt" : "ikke betalt") . "</td></tr>";
+
         if ($row["verdict"] == 3) {
             $losings += 5 * (1 - $row["paid"]);
-        } else if ($row["to"] == $id) {
-            if($row["verdict"] == 1) {
+        } elseif ($row["to"] == $id) {
+            if ($row["verdict"] == 1) {
                 $losings += 5 * (1 - $row["paid"]);
             }
-        } else if($row["from"] == $id) {
-            if($row["verdict"] == 2) {
+        } elseif ($row["from"] == $id) {
+            if ($row["verdict"] == 2) {
                 $losings += 5 * (1 - $row["paid"]);
             }
         }
     }
-    $klandring_table .= "</tbody></table>";    
+    $klandring_table .= "</tbody></table>";
 }
 ?>
 <div class="panel panel-default">
