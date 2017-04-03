0e10b9585a35df55f814f6c7f0a4eac259a71187
Erik Hoyjor <ErikHoyJor@gmail.com>
2017-04-03 19:53:24 +0200

    cleaned up login a bit.

--- source/routes/login.php
+++ source/routes/login.php
@@ -1,9 +1,5 @@
-<?php
-echo '<a href="';
-
+<a href="<?php
 echo OAUTH_PROVIDER . "authorize?client_id=" . OAUTH_CLIENT_ID 
                     . "&amp;response_type=code"
                     . "&amp;redirect_uri=http://$_SERVER[HTTP_HOST]/oauth-callback.php";
-
-echo '"><h3>Login</h3></a>';
-?>
+?>"><h3>Login</h3></a>
