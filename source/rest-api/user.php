686cf6874cc67aaeb1f79289e2ee47854655c1bb
Erik Hoyjor <ErikHoyJor@gmail.com>
2017-11-30 15:13:36 +0100

    added get user api

---
+++ source/rest-api/user.php
@@ -0,0 +1,32 @@
+<?php
+require("rest-helper.php");
+header("Content-Type: application/json; charset=UTF-8");
+
+if (validate() == FALSE) {
+    require("403.php");
+    die();
+}
+
+if (isset($_GET["id"])) {
+    $ids_str = $_GET["id"];
+
+    if (is_numeric($ids_str)) {
+        $ids = $_GET["id"];
+    } else {
+        $ids = array_map("intval", array_filter(explode(',', $ids_str), "is_numeric"));
+        if (count($ids) == 0) {
+            require("400.php");
+            die();
+        }
+    }
+    
+    echo json_encode(get_user_infos_arr($ids));
+
+} else {
+    require("400.php");
+    die();
+}
+
+
+$db->close();
+?>
